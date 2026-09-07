d434195fc3d235ad179994a47b9c465e62ff37c9
SHMEL-BZZ <ekaterinaandreeva370@gmail.com>
2026-09-07 11:24:29 +0300

    Создана общая структура серверной и клиентской части. Созданы первоначально необходимые для разработки файлы, добавлено подключение к БД.

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+// для запуска client части, команда npm start в папке client в терминале
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          Working
+      </header>
+    </div>
+  );
+}
+
+export default App;
