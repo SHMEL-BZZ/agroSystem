e3e61b2ec0fb32546d00050b63eb69c96c112550
animarcko <leraandrita@gmail.com>
2026-09-10 16:23:05 +0300

    Проверка подключения к БД

--- server/db.js
+++ server/db.js
@@ -1,14 +1,16 @@
 // файл для взаимодействия с базой данных (берет данные из .env)
-const {Sequelize} = require('sequelize');
+require('dotenv').config()  
 
-module.exports = new Sequelize (
+const { Sequelize } = require('sequelize')
+
+module.exports = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USERNAME,
     process.env.DB_PASSWORD,
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        port: Number(process.env.DB_PORT) || 5432,
         logging: false,
     }
 )
