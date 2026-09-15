24aa20bbc2f05367c9c512daef5d1a50b6d3889e
andreeva ekaterina <ekaterinaandreeva370@gmail.com>
2026-09-15 10:50:45 +0300

    Добавлена регистрация (userRouter и userController) с ипосльзованием токенов, обработчики ошибок, добавлены зависимости в index.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,8 +1,18 @@
-const ApiError = require('../error/ApiError');
+const jwt = require('jsonwebtoken')
 
-module.exports = function (err, req, res, next) {
-    if (err instanceof ApiError) {
-        return res.status(err.status).json({message: err.message})
+module.exports = function (req, res, next) {
+    if (req.method === "OPTIONS") {
+        next()
     }
-    return res.status(500).json({message: "Непредвиденная ошибка!"})
-}
+    try {
+        const token = req.headers.authorization.split(' ')[1] // Bearer
+        if (!token) {
+            return res.status(401).json({message: "Не авторизован"})
+        }
+        const decoded = jwt.verify(token, process.env.SECRET_KEY)
+        req.user = decoded
+        next()
+    } catch (e) {
+        res.status(401).json({message: "Не авторизован"})
+    }
+};
