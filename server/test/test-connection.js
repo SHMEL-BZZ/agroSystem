bf4830f6ba43fc7ad01682bb7e6db7bc6fbce2ec
andreeva ekaterina <ekaterinaandreeva370@gmail.com>
2026-09-16 10:00:53 +0300

    Скорректирована структура проекта: удалены ненужные файлы, вынесены в отдельный модуль генераторы и тесты.

--- server/test/test-connection.js
+++ server/test/test-connection.js
@@ -1,8 +1,8 @@
-const { User } = require('./models/models')
+const { User } = require('../models/models')
 
 async function demo() {
     const users = await User.findAll()
-    console.log('👤 Пользователи:')
+    console.log('Пользователи:')
     users.forEach(u => console.log(`  #${u.id}  ${u.login}`))
     process.exit(0)
 }
