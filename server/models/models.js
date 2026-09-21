ba4a4f7955b88fa62e6a8361f4eb7871c7d375af
animarcko <leraandrita@gmail.com>
2026-09-21 11:56:39 +0300

    Добавление обработки почты при регистрации пользователя

--- server/models/models.js
+++ server/models/models.js
@@ -2,7 +2,6 @@ const sequelize = require('../db')
 const {DataTypes} = require('sequelize')
 
 // модели
-// пользователи
 const User = sequelize.define('User', {
     id: {
         type: DataTypes.INTEGER,
@@ -20,6 +19,12 @@ const User = sequelize.define('User', {
         type: DataTypes.STRING(255),
         allowNull: false,
         field: 'пароль'
+    },
+    email: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+        unique: true,
+        field: 'почта'         
     }
 }, {
     tableName: 'пользователи',
